d14b32c3b71e1fc889bda0c2dafcf0e096b47fbf
Miguel Panuto <miguel.panuto@live.com>
2020-07-20 03:23:41 -0300

    struct with query and the filename

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,28 @@ fn main() {
     let args:Vec<String> = env::args().collect(); // That will read the user entry args
     
     // Pointer must be pass, not pass the clone and maintain the ownership
-    let (query, filename) = parse_config(&args); // Return tuple, the query and filename
+    let config = parse_config(&args); // Return tuple, the query and filename
     // By now we have the array: [where is compiled, query, filename]
 
-    println!("Searching for {}", query);
-    println!("Inside: {}", filename);
+    println!("Searching for {}", config.query);
+    println!("Inside: {}", config.filename);
 
-    let contents = fs::read_to_string(filename) // This function reads a outside file
+    let contents = fs::read_to_string(config.filename) // This function reads a outside file
         .expect("Something went wrong while reading the file"); // the filename is the path to
 
     println!("With text:\n {}", contents);
 }
 
+struct Config { // Can be stored inside a struct now
+    query: String, // The code will be more controlled
+    filename: String,
+}
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1]; // This will save what we looking for inside this variable
-    let filename = &args[2]; // And this the filename
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone(); // This will save what we looking for inside this variable
+    // Cannot borrow the string, the clone guarantees that, but in the change for more simplicity
+    // a little of performance give it away
+    let filename = args[2].clone(); // And this the filename
 
-    (query, filename) // Return those 2
+    Config { query, filename } // Return a config struct
 }
