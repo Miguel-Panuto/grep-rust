913118af60dcc6169c97e6478ee3f4ac792440c7
Miguel Panuto <miguel.panuto@live.com>
2020-07-20 03:06:11 -0300

    Adding outside file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env; // Environment lib
+use std::fs;
 
 fn main() {
     let args:Vec<String> = env::args().collect(); // That will read the user entry args
@@ -9,4 +10,9 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("Inside: {}", filename);
+
+    let contents = fs::read_to_string(filename) // This function reads a outside file
+        .expect("Something went wrong while reading the file"); // the filename is the path to
+
+    println!("With text:\n {}", contents);
 }
