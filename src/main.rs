b5432378006ef669873538f5940b45b6e954b600
Miguel Panuto <miguel.panuto@live.com>
2020-07-20 04:35:38 -0300

    no err output text

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect(); // That will read the user entry args
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err); // Handle in error case
+        eprintln!("Problem parsing arguments: {}", err); // Handle in error case
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("Inside: {}", config.filename);
-
     if let Err(e) = grep_prop::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
