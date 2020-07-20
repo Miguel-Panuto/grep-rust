6c760a02116a971d72b2a2459ae0f80664de00bf
Miguel Panuto <miguel.panuto@live.com>
2020-07-20 02:55:04 -0300

    saving the variables

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,11 @@ use std::env; // Environment lib
 
 fn main() {
     let args:Vec<String> = env::args().collect(); // That will read the user entry args
-    print!("{:?}", args); // Print the args
+    
+    let query = &args[1]; // This will save what we looking for inside this variable
+    let filename = &args[2]; // And this the filename
+    // By now we have the array: [where is compiled, query, filename]
+
+    println!("Searching for {}", query);
+    println!("Inside: {}", filename);
 }
