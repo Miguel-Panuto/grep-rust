d7de51b03a33ce62e21a0c830866e05d94e8ac25
Miguel Panuto <miguel.panuto@live.com>
2020-07-20 03:15:14 -0300

    cofigs parse, tupple return

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::fs;
 fn main() {
     let args:Vec<String> = env::args().collect(); // That will read the user entry args
     
-    let query = &args[1]; // This will save what we looking for inside this variable
-    let filename = &args[2]; // And this the filename
+    // Pointer must be pass, not pass the clone and maintain the ownership
+    let (query, filename) = parse_config(&args); // Return tuple, the query and filename
     // By now we have the array: [where is compiled, query, filename]
 
     println!("Searching for {}", query);
@@ -15,4 +15,12 @@ fn main() {
         .expect("Something went wrong while reading the file"); // the filename is the path to
 
     println!("With text:\n {}", contents);
+}
+
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1]; // This will save what we looking for inside this variable
+    let filename = &args[2]; // And this the filename
+
+    (query, filename) // Return those 2
 }
